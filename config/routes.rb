cf61666a5a19638c13f6a598f47ca1beb278c077
Sara Gorecki <sgorecki@gmail.com>
2014-01-09 15:09:18 -0500

    add routes for amoebas, acts, and talents

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AmoebaCircus::Application.routes.draw do
+
+  resources :amoebas
+  resources :acts
+  resources :talents
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
