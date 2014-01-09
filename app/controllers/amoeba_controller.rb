9858a917ecae1138ba517c3d5be4ba69a8a5c70a
Sara Gorecki <sgorecki@gmail.com>
2014-01-09 16:29:42 -0500

    make controllers for amoeba and act

---
+++ app/controllers/amoeba_controller.rb
@@ -0,0 +1,41 @@
+class AmoebaController < ApplicationController
+  def index
+    @amoebas = Amoeba.all
+  end
+
+  def new
+    @amoeba = Amoeba.new
+  end
+
+  def create
+    @amoeba = Amoeba.new(params[:amoeba])
+    if @amoeba.save
+      redirect @amoeba
+    else
+      render new
+    end
+  end
+
+  def show
+    @amoeba = Amoeba.find(params[:id])
+  end
+
+  def edit
+    @amoeba = Amoeba.find(params[:id])
+  end
+
+  def update
+    @amoeba = Amoeba.find(params[:id])
+    if @amoeba.update_attributes(params[:amoeba])
+      redirect @amoeba
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @amoeba = Amoeba.find(params[:id])
+    @amoeba.destroy
+  end
+
+end
