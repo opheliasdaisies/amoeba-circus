eee73143605ba12a9a5315dccf9d49286a8f7bdc
Sara Gorecki <sgorecki@gmail.com>
2014-01-10 12:20:50 -0500

    fix redirects in acts and amoebas for destroy

---
+++ app/controllers/amoebas_controller.rb
@@ -0,0 +1,42 @@
+class AmoebasController < ApplicationController
+  def index
+    @amoebas = Amoeba.all
+  end
+
+  def new
+    @amoeba = Amoeba.new
+  end
+
+  def create
+    @amoeba = Amoeba.new(params[:amoeba])
+    if @amoeba.save
+      redirect_to @amoeba
+    else
+      render new
+    end
+  end
+
+  def show
+    @amoeba = Amoeba.find(params[:id])
+  end
+
+  def edit
+    @amoeba = Amoeba.find(params[:id])
+  end
+
+  def update
+    @amoeba = Amoeba.find(params[:id])
+    if @amoeba.update_attributes(params[:amoeba])
+      redirect_to @amoeba
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @amoeba = Amoeba.find(params[:id])
+    @amoeba.destroy
+    redirect_to amoebas_url
+  end
+
+end
